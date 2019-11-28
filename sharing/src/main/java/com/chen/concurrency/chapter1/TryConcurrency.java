acb96fee074ff662339e3c87056812094959ef0a
itheima <786814380@qq.com>
2019-11-28 15:05:03 +0800

    one

---
+++ sharing/src/main/java/com/chen/concurrency/chapter1/TryConcurrency.java
@@ -0,0 +1,57 @@
+package com.chen.concurrency.chapter1;
+
+public class TryConcurrency {
+
+    public static void main(String[] args) {
+
+        new Thread("READ-Thread"){
+            @Override
+            public void run() {
+                readFromDataBase();
+            }
+        }.start();
+
+        new Thread("WRITE-Thread"){
+            @Override
+            public void run() {
+                writeDataToFile();
+            }
+        }.start();
+
+//        try {
+//            Thread.sleep(1000 * 300L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        readFromDataBase();
+//        writeDataToFile();
+    }
+
+    private static void readFromDataBase(){
+        try {
+            printIn("Begin read data from db");
+            Thread.sleep(1000 * 30L);
+            printIn("Read data done and start handle it.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        printIn("处理完并且是成功的");
+    }
+
+    private static void writeDataToFile(){
+        try {
+            printIn("Begin write data to File");
+            Thread.sleep(1000 * 20L);
+            printIn("Write data done and start handle it.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        printIn("处理完并且是成功的");
+    }
+
+    private static void printIn(String msg){
+        System.out.println(msg);
+    }
+}
