953dee7fca1bdb5cd2e11c2f495e629c39f5bcc6
itheima <786814380@qq.com>
2019-11-29 11:40:45 +0800

    4

--- sharing/src/main/java/com/chen/concurrency/chapter2/TicketWindow.java
+++ sharing/src/main/java/com/chen/concurrency/chapter2/TicketWindow.java
@@ -2,8 +2,24 @@ package com.chen.concurrency.chapter2;
 
 public class TicketWindow extends Thread {
 
+    private final String name;
+
+    private static final int max = 50;
+
+    private static int index = 1;
+
+    public TicketWindow(String name) {
+        this.name = name;
+    }
+
     @Override
     public void run() {
-        super.run();
+
+        while (index <= max){
+
+            System.out.println("柜台："+name+"当前的号码是：" + (index++));
+
+        }
+
     }
 }
